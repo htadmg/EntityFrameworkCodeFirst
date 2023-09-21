389725f2458102e5ea2d916b0ef085edd7c23593
Agata Domingues <124289385+htadmg@users.noreply.github.com>
2023-09-21 13:20:39 -0300

    Add project files.

---
+++ EntityFrameworkCodeFirst/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCodeFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
