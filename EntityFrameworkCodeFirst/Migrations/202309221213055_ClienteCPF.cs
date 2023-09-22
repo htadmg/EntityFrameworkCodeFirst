e2f6b5e3f1c02d2f06d6d41b786a8110ecc38bbd
Agata Domingues <124289385+htadmg@users.noreply.github.com>
2023-09-22 14:53:52 -0300

    att configuração de conexao

---
+++ EntityFrameworkCodeFirst/Migrations/202309221213055_ClienteCPF.cs
@@ -0,0 +1,18 @@
+﻿namespace EntityFrameworkCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ClienteCPF : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Clientes", "CPF", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Clientes", "CPF");
+        }
+    }
+}
---
+++ EntityFrameworkCodeFirst/Migrations/202309221227502_Produto.cs
@@ -0,0 +1,18 @@
+﻿namespace EntityFrameworkCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Produto : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.Produtoes", newName: "Produtos");         
+        }
+        
+        public override void Down()
+        {
+            RenameTable(name: "dbo.Produtoes", newName: "Produtos");
+        }
+    }
+}
